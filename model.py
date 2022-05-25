cad3dc68d4ec14e585451013e4d16fbc70dc4512
Leham Greeves <Leham.Greeves@gmail.com>
2022-05-25 22:24:26 +0200

    finally whooowhooo!

--- model.py
+++ model.py
@@ -58,31 +58,42 @@ def _preprocess_data(data):
     # ---------------------------------------------------------------
 
     # ----------- Replace this code with your own preprocessing steps --------
-    #changing time to date time
-    for x in feature_vector_df.columns:
-        if x == np.nan:
-            x.replace(feature_vector_df.mean())
-        return x
-    feature_vector_df['time'] = pd.to_datetime(feature_vector_df['time'])
+    feature_vector_df['Valencia_wind_deg'] = feature_vector_df.Valencia_wind_deg.map({'level_5':5, 'level_10':10, 'level_9':9, 'level_8':8, 
+                                                                        'level_7':7, 'level_6':6, 'level_4':4, 'level_3':3, 
+                                                                        'level_1':1, 'level_2':2})
 
-    feature_vector_df['Year'] = feature_vector_df['time'].dt.year
-    feature_vector_df['Month'] = feature_vector_df['time'].dt.month
-    feature_vector_df['Day'] = feature_vector_df['time'].dt.day
-    feature_vector_df['Hour'] = feature_vector_df['time'].dt.hour
-    feature_vector_df['Minute'] = feature_vector_df['time'].dt.minute
-    feature_vector_df['Second'] = feature_vector_df['time'].dt.second
-    feature_vector_df = feature_vector_df.drop(['Barcelona_temp', 'Barcelona_temp_max', 'Barcelona_temp_min', 'Bilbao_temp', 'Bilbao_temp_max', 'Bilbao_temp_min', 'Madrid_temp', 'Madrid_temp_max', 'Madrid_temp_min', 'Seville_temp', 'Seville_temp_min', 'Valencia_temp', 'Valencia_temp_min','time', 'Unnamed: 0'],axis=1)
-    # replacing null values with median
-    feature_vector_df['Valencia_pressure'] = feature_vector_df['Valencia_pressure'].fillna(feature_vector_df['Valencia_pressure'].median())
 
-    # Changing Object dtypes to int
-    feature_vector_df['Valencia_wind_deg'] = feature_vector_df['Valencia_wind_deg'].str.extract('(\d+)')  
-    feature_vector_df['Valencia_wind_deg'] = pd.to_numeric(feature_vector_df['Valencia_wind_deg'])
+    feature_vector_df['Seville_pressure'] = feature_vector_df.Seville_pressure.map({'sp25':25, 'sp23':23, 'sp24':24, 'sp21':21, 'sp16':16, 'sp9':9, 'sp15':15, 'sp19':19, 'sp22':22, 'sp11':11,
+                                                                        'sp8':8, 'sp4':4, 'sp6':6, 'sp13':13, 'sp17':17, 'sp20':20, 'sp18':18, 'sp14':14, 'sp12':12, 'sp5':5, 'sp10':10,
+                                                                        'sp7':7, 'sp3':3, 'sp2':2, 'sp1':1})
 
-    # Convert to integer
-    feature_vector_df['Seville_pressure'] = feature_vector_df['Seville_pressure'].str.extract('(\d+)')   
-    feature_vector_df['Seville_pressure'] = pd.to_numeric(feature_vector_df['Seville_pressure'])
-    
+
+
+    # Creating new features
+
+    feature_vector_df['hour']  = feature_vector_df['time'].astype('datetime64').dt.hour
+    feature_vector_df['day']  = feature_vector_df['time'].astype('datetime64').dt.day
+    feature_vector_df['month'] = feature_vector_df['time'].astype('datetime64').dt.month
+    feature_vector_df['year']  = feature_vector_df['time'].astype('datetime64').dt.year
+    # Dropping features with high collinearity
+
+    feature_vector_df = feature_vector_df.drop(['Unnamed: 0', 'time','Madrid_wind_speed',
+                    'Seville_humidity','Valencia_pressure','Seville_temp_max',
+                    'Madrid_pressure','Valencia_temp_max','Valencia_temp',
+                    'Seville_temp','Valencia_temp_min','Barcelona_temp_max',
+                    'Madrid_temp_max','Barcelona_temp', 'Bilbao_temp_min',
+                    'Bilbao_temp','Barcelona_temp_min', 'Bilbao_temp_max',
+                    'Seville_temp_min','Madrid_temp', 'Madrid_temp_min',
+                    'Bilbao_rain_1h','Bilbao_clouds_all',
+                    'Seville_clouds_all','Madrid_clouds_all','Barcelona_rain_1h',
+                    'Seville_rain_1h','Bilbao_snow_3h','Seville_rain_3h',
+                    'Madrid_rain_1h','Barcelona_rain_3h','Valencia_snow_3h',
+                    'Barcelona_pressure','Valencia_wind_speed','Seville_weather_id',
+                    'Madrid_weather_id','Barcelona_weather_id','Bilbao_weather_id',
+                    'Bilbao_wind_speed','Seville_wind_speed','Barcelona_wind_speed'], axis = 1)
+        
+    predict_vector = feature_vector_df
+       
     predict_vector = feature_vector_df
     # ------------------------------------------------------------------------
 
@@ -132,4 +143,4 @@ def make_prediction(data, model):
     # Perform prediction with model and preprocessed data.
     prediction = model.predict(prep_data)
     # Format as list for output standardisation.
-    return prediction[0].tolist()
+    return prediction.tolist()
